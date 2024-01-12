0a77a690c89336dd3db180a57e967c9ef041ae34
Garrett <garwad9@gmail.com>
2024-01-11 18:00:45 -0600

    Building system worked on

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public GameObject BuilderPos;
+    public GameObject Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Builder Movement
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+        }
+
+        if (Input.GetKeyDown((KeyCode.S)))
+        {
+            transform.position += Vector3.back;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            transform.position += Vector3.down;
+        }
+
+        //Creating game object
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(Material, transform.position, Quaternion.identity);
+        }
+    }
+}
+
