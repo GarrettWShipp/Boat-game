5a93c2bab6dd3fde44b031308c718864f2aabef8
Garrett Shipp <garwad9@gmail.com>
2024-02-22 14:33:00 -0600

    center?

--- Assets/FindCenterPoint.cs
+++ Assets/FindCenterPoint.cs
@@ -1,30 +1,51 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FindCenterPoint : MonoBehaviour
 {
+    public BoxCollider center;
+    public List<Transform> children = new List<Transform>();
+    Vector3 avgVec;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Center = Vector3.zero;
+        //center.center = Vector3.zero;
+        avgVec = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.root.gameObject == transform.gameObject)
+        
+        if (transform.childCount > 0)
         {
-            
-            if (transform.childCount > 0)
+            avgVec = Vector3.zero;
+            foreach (Transform pos in children)
             {
-                var allChildren = transform.gameObject.GetComponentsInChildren(Transform);
-                foreach (Transform child in allChildren)
-                {
-                    Center += child.transform.position;
-                }
-                Center /= (allChildren.length);
+
+                avgVec.x += pos.localPosition.x;
+                avgVec.y += pos.localPosition.y;
+                avgVec.z += pos.localPosition.z;
+
             }
+            center.center = (avgVec / children.Count);
+            
+            children = transform.gameObject.GetComponentsInChildren<Transform>().ToList<Transform>();
+            /*foreach (Transform child in children)
+            {
+                if (child.name == gameObject.name)
+                {
+                    children.Remove(child);
+                }
+                center.size += child.transform.position;
+            }*/
+            //center.center = children.Average();
+            //Center.size /= (Children.Length);
+            
+
         }
     }
 }
