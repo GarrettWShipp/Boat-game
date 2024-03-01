3d86be0cc98a0ac37a6a411cc0ea198570eaf8ef
Garrett Shipp <garwad9@gmail.com>
2024-02-29 21:25:28 -0600

    Box collider size mostly done
    needs fine tuning

--- Assets/FindCenterPoint.cs
+++ Assets/FindCenterPoint.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,10 +9,15 @@ public class FindCenterPoint : MonoBehaviour
     public BoxCollider center;
     public List<Transform> children = new List<Transform>();
     Vector3 avgVec;
+    public int childCounter = 0;
+    public int counter = 0;
+    private float maxX = 0, maxY = 0, maxZ = 0, minX = 0, minY = 0, minZ = 0;
+    Vector3 disVec;
     
     // Start is called before the first frame update
     void Start()
     {
+        center.size = Vector3.zero;
         center.center = Vector3.zero;
         avgVec = new Vector3(0.0f, 0.0f, 0.0f);
     }
@@ -19,9 +25,13 @@ public class FindCenterPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        children = transform.Cast<Transform>().ToList();
         
-        if (transform.childCount > 0)
+        childCounter = children.Count;
+
+        if (childCounter != counter)
         {
+            //find center
             avgVec = Vector3.zero;
             foreach (Transform pos in children)
             {
@@ -32,20 +42,53 @@ public class FindCenterPoint : MonoBehaviour
 
             }
             center.center = (avgVec / children.Count);
-            
-            children = transform.gameObject.GetComponentsInChildren<Transform>().ToList<Transform>();
-            /*foreach (Transform child in children)
-            {
-                if (child.name == gameObject.name)
-                {
-                    children.Remove(child);
-                }
-                center.size += child.transform.position;
-            }*/
-            //center.center = children.Average();
-            //Center.size /= (Children.Length);
-            
 
+            //adjust box collider
+            foreach (Transform child in children)
+            {
+                if (child.localPosition.x > maxX)
+                {
+                    maxX = child.localPosition.x;
+                }
+
+                if (child.localPosition.x < minX)
+                {
+                    minX = child.localPosition.x;
+                }
+
+                if (child.localPosition.y > maxY)
+                {
+                    maxY = child.localPosition.y;
+                }
+
+                if (child.localPosition.y < minY)
+                {
+                    minY = child.localPosition.y;
+                }
+
+                if (child.localPosition.z > maxZ)
+                {
+                    maxZ = child.localPosition.z;
+                }
+
+                if (child.localPosition.z < minZ)
+                {
+                    minZ = child.localPosition.z;
+                }
+
+                disVec.x = Mathf.Sqrt(Mathf.Exp(2) + (maxX - minX));
+                disVec.y = Mathf.Sqrt(Mathf.Exp(2) + (maxY - minY));
+                disVec.z = Mathf.Sqrt(Mathf.Exp(2) + (maxZ - minZ));
+                center.size = disVec;
+            }
+            if (childCounter <= counter)
+            {
+                counter--;
+            }
+            if(childCounter >= counter)
+            {
+                counter++;
+            }
         }
     }
 }
